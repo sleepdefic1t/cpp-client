3c4c474274cf2343f3326f4fe00566d9f1aa87ac
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2021-01-14 20:43:47 -0500

    Update connection.cpp

--- test/connection/connection.cpp
+++ test/connection/connection.cpp
@@ -12,20 +12,26 @@ constexpr auto ADN_API_PORT = 4003;
 TEST(api_connection, constructor_default) {
   Connection<Api> connection;
 
-  ASSERT_TRUE(strcmp(connection.getPeer().c_str(), DEFAULT_PEER_ADN) == 0);
+  const auto result = connection.getPeer();
+
+  ASSERT_TRUE(strcmp(result.c_str(), DEFAULT_PEER_ADN) == 0);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 TEST(api_connection, constructor_custom_peer) {
   Connection<Api> connection(DEFAULT_PEER_APN);
 
-  ASSERT_TRUE(strcmp(connection.getPeer().c_str(), DEFAULT_PEER_APN) == 0);
+  const auto result = connection.getPeer();
+
+  ASSERT_TRUE(strcmp(result.c_str(), DEFAULT_PEER_APN) == 0);
 }
 
 TEST(api_connection, constructor_ip_port) {
   Connection<Api> connection(ADN_PEER, ADN_API_PORT);
 
   constexpr auto expected = "167.114.29.55:4003";
+  const auto result = connection.getPeer();
 
-  ASSERT_TRUE(strcmp(connection.getPeer().c_str(), expected) == 0);
+  printf("\n\nresult: %s", result.c_str());
+  ASSERT_TRUE(strcmp(result.c_str(), expected) == 0);
 }
